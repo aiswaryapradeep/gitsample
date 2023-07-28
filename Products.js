61421653089550e418883c653e1b7ccd2bbb0f63
user <aiswaryapradeep003@gmail.com>
2023-07-28 08:15:18 +0530

    test

---
+++ Products.js
@@ -0,0 +1,40 @@
+import product1 from "./assets/img1.jpg"
+import product2 from "./assets/5.webp"
+import product3 from "./assets/4.webp"
+import product4 from "./assets/3.webp"
+import product5 from "./assets/img2.jpg"
+
+
+
+export const Products = [
+    {
+        id: 1,
+        name: "Jeans",
+        price : 200,
+        image: product1
+    },
+    {
+        id: 2,
+        name: "Jeans1",
+        price : 200,
+        image: product2
+    },
+    {
+        id: 3,
+        name: "Jeans2",
+        price : 200,
+        image: product3
+    },
+    {
+        id: 4,
+        name: "tops",
+        price : 200,
+        image: product4
+    },
+    {
+        id: 5,
+        name: "tops2",
+        price : 200,
+        image: product5
+    },
+]
